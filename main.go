db0c83ac9b5f970c241d552f7bee14d50ae3508d
Andrew Arrow <oneone@gmail.com>
2020-11-07 15:01:52 -0800

    new idea

--- main.go
+++ main.go
@@ -3,29 +3,23 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"time"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	i := 0
-	db, _ := sql.Open("mysql", "root:123456@/")
-	defer db.Close()
-
-	var version string
-	db.QueryRow("SELECT VERSION()").Scan(&version)
-	fmt.Println("Connected to:", version)
-
-	for {
-		fmt.Println("hi-test3", i)
-		i++
-		time.Sleep(time.Second * 5)
-		db, err := sql.Open("mysql", "root:123456@/")
-		defer db.Close()
-
-		var version string
-		db.QueryRow("SELECT NOW() as version").Scan(&version)
-		fmt.Println("Connected to:", version, err)
+	cnn, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/test_db")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	id := 1
+	var name string
+
+	if err := cnn.QueryRow("SELECT name FROM test_tb WHERE id = ? LIMIT 1", id).Scan(&name); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(id, name)
 }
