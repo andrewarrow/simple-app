83da6c8a6725c96b76b5ba1f7c7a9a1d6cba53d7
Andrew Arrow <oneone@gmail.com>
2020-11-07 13:37:27 -0800

    mysql

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	i := 0
+	db, _ := sql.Open("mysql", "root:123456@/")
+	defer db.Close()
+
+	var version string
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	fmt.Println("Connected to:", version)
+
 	for {
 		fmt.Println("hi-test2", i)
 		i++
